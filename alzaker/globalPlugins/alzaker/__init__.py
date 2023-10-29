c1efe37cc7c5707a4bc7648dc0b6a01c29e052ab
Ahmed Samy El-khouly <ahmedthebest31@gmail.com>
2023-10-29 17:22:20 +0200

    Update __init__.py
    fix the strings and add more shortcut
    
    Co-Authored-By: mesteranas <135442710+mesteranas@users.noreply.github.com>

--- alzaker/globalPlugins/alzaker/__init__.py
+++ alzaker/globalPlugins/alzaker/__init__.py
@@ -1,9 +1,4 @@
-"""
-this file typed by mesteranas
-email:anasformohammed@gmail.com
-github:https://github.com/mesteranas/
-thankyou ahmed samy (ahmedthebest) to give me sounds ,texts and type readme file
-"""
+import api
 from wx.adv import NotificationMessage as NM
 import webbrowser as web
 import os
@@ -16,8 +11,8 @@ import gui
 import globalPluginHandler
 import ui
 from threading import Thread
+import tones
 from time import sleep
-from winsound import PlaySound
 from scriptHandler import script
 import addonHandler
 addonHandler.initTranslation()
@@ -29,18 +24,24 @@ confspec = {
 
 config.conf.spec[roleSECTION] = confspec
 path=os.path.join(os.path.abspath(os.path.dirname(__file__)), "ad")
+zkr="None"
 def allah ():
+	global zkr
 	if config.conf[roleSECTION]["d"] >0:
 		while True:
 			sleep(config.conf[roleSECTION]["d"]*60)
 			if config.conf[roleSECTION]["type"]==0:
 				with open(path + "/text.txt","r",encoding="utf-8") as file:
-					ui.message(choice(file.read().split("\n")))
+					ass=choice(file.read().split("\n"))
+					ui.message(ass)
+					zkr=ass
 			elif config.conf[roleSECTION]["type"]==1:
 				nvwave.playWaveFile(os.path.join(path,"sounds",choice(os.listdir(path + "/sounds"))),1)
 			else:
 				with open(path + "/text.txt","r",encoding="utf-8") as file:
-					NM(_("alzaker"),choice(file.read().split("\n"))).Show(50)
+					ass=choice(file.read().split("\n"))
+					NM(_("alzaker"),ass).Show(50)
+					zkr=ass
 
 t=Thread(target=allah)
 t.start()
@@ -48,11 +49,11 @@ class CRSettingsPanel(SettingsPanel):
 	title = _("alzaker")
 	def makeSettings(self, settingsSizer):
 		sHelper = guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
-		self.tlable = sHelper.addItem(wx.StaticText(self, label=_("type"), name="ts"))
+		self.tlable = sHelper.addItem(wx.StaticText(self, label=_("select reminder type"), name="ts"))
 		self.sou= sHelper.addItem(wx.Choice(self, name="ts"))
-		self.tlable1 = sHelper.addItem(wx.StaticText(self, label=_("duration"), name="ts1"))
+		self.tlable1 = sHelper.addItem(wx.StaticText(self, label=_("select interval between each remembrances buy minutes"), name="ts1"))
 		self.sou1= sHelper.addItem(wx.SpinCtrl(self, name="ts1",min=0,max=500))
-		self.sou.Set([_("speak"),_("sound"),_("Notification")])
+		self.sou.Set([_("by TTS"),_("by audio files"),_("as a windows Notification")])
 		self.sou.SetSelection(config.conf[roleSECTION]["type"])
 		self.sou1.SetValue(config.conf[roleSECTION]["d"])
 		donate=sHelper.addItem(wx.Button(self,label=_("donate")))
@@ -70,8 +71,16 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	scriptCategory= _("alzaker")
 	@script(gesture="kb:NVDA+alt+z")
 	def script_toggle(self,gesture):
+		global zkr
 		with open(path + "/text.txt","r",encoding="utf-8") as file:
-			ui.message(choice(file.read().split("\n")))
-	script_toggle.__doc__= _("view random 1")
+			ass=choice(file.read().split("\n"))
+			ui.message(ass)
+			zkr=ass
+	script_toggle.__doc__= _("say a random remembrances")
+	@script(gesture="kb:NVDA+alt+x")
+	def script_toggle1(self,gesture):
+		if api.copyToClip(zkr):
+			ui.message(_("copied"))
+	script_toggle1.__doc__= _("coppy it to  system clipboard")
 	def terminate(self):
 		NVDASettingsDialog.categoryClasses.remove(CRSettingsPanel)
