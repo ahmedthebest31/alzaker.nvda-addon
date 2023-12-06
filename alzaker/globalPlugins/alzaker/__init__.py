f685c3f78998bd648945588fa4cad9e30ca84ffb
The-fake-psychologist <aidinrakhsha95@gmail.com>
2023-12-07 00:08:30 +0330

    Fix bugs related to remembrences
    Prevented the addon from spamming with remembrances if the interval is set to 0. The code for it existed, but didn't work for some reason. Insert Alt X does not copy None anymore.

--- alzaker/globalPlugins/alzaker/__init__.py
+++ alzaker/globalPlugins/alzaker/__init__.py
@@ -27,23 +27,25 @@ path=os.path.join(os.path.abspath(os.path.dirname(__file__)), "ad")
 zkr="None"
 def allah ():
 	global zkr
-	if config.conf[roleSECTION]["d"] >0:
-		while True:
-			sleep(config.conf[roleSECTION]["d"]*60)
-			if config.conf[roleSECTION]["type"]==0:
-				with open(path + "/text.txt","r",encoding="utf-8") as file:
-					ass=choice(file.read().split("\n"))
-					ui.message(ass)
-					zkr=ass
-			elif config.conf[roleSECTION]["type"]==1:
-				nvwave.playWaveFile(os.path.join(path,"sounds",choice(os.listdir(path + "/sounds"))),1)
-			else:
-				with open(path + "/text.txt","r",encoding="utf-8") as file:
-					ass=choice(file.read().split("\n"))
-					NM(_("alzaker"),ass).Show(50)
-					zkr=ass
+	while True:
+		#Important note: if we put the while loop under the below condition, the loop starts if the reminder interval is not 0, but it'll continue running even if the value of the interval changes back to 0, thus; it results in the addon spamming with remembrances!
+		# Returnes if the interval is set to 0. The reason for this was explained in the above line.
+		if not config.conf[roleSECTION]["d"]: return
+		sleep(config.conf[roleSECTION]["d"]*60)
+		if config.conf[roleSECTION]["type"]==0:
+			with open(path + "/text.txt","r",encoding="utf-8") as file:
+				ass=choice(file.read().split("\n"))
+				ui.message(ass)
+				zkr=ass
+		elif config.conf[roleSECTION]["type"]==1:
+			nvwave.playWaveFile(os.path.join(path,"sounds",choice(os.listdir(path + "/sounds"))),1)
+		else:
+			with open(path + "/text.txt","r",encoding="utf-8") as file:
+				ass=choice(file.read().split("\n"))
+				NM(_("alzaker"),ass).Show(50)
+				zkr=ass
 
-t=Thread(target=allah)
+t=Thread(target=allah, daemon=True)
 t.start()
 class CRSettingsPanel(SettingsPanel):
 	title = _("alzaker")
@@ -61,8 +63,18 @@ class CRSettingsPanel(SettingsPanel):
 	def postInit(self):
 		self.sou.SetFocus()
 	def onSave(self):
+		ShouldCall=False
+		if config.conf[roleSECTION]["d"]==0 and self.sou1.Value>0:
+			ShouldCall = True
+		else:
+			ShouldCall = False
 		config.conf[roleSECTION]["type"]=self.sou.Selection
 		config.conf[roleSECTION]["d"]=self.sou1.Value
+		config.conf.save()
+		if ShouldCall:
+			t=Thread(target=allah, daemon=True)
+			t.start()
+
 	def ondonate(self,e):
 		ui.message("please wait")
 		web.open("https://www.paypal.me/ahmedthebest31")
@@ -77,10 +89,18 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			ui.message(ass)
 			zkr=ass
 	script_toggle.__doc__= _("say a random remembrances")
+
+
 	@script(gesture="kb:NVDA+alt+x")
 	def script_toggle1(self,gesture):
-		if api.copyToClip(zkr):
-			ui.message(_("copied"))
+		if zkr is not "None":
+			if api.copyToClip(zkr):
+				ui.message(_("copied"))
+
+		else:
+			ui.message ("Could not find any previously generated remembrance to copy!")
 	script_toggle1.__doc__= _("coppy it to  system clipboard")
+
+
 	def terminate(self):
 		NVDASettingsDialog.categoryClasses.remove(CRSettingsPanel)
